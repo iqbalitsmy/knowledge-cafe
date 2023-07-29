73ccd2bbcfbafc07537a8a405fa4213836587f2c
tatamax <tatamax0711@gmail.com>
2023-07-29 16:22:01 +0600

    Toast added

--- knowledge-cafe/src/Components/Blogs/Blogs.jsx
+++ knowledge-cafe/src/Components/Blogs/Blogs.jsx
@@ -2,21 +2,26 @@ import React, { useEffect, useState } from 'react';
 import './Blogs.css'
 import Blog from '../Blog/Blog';
 import Aside from '../Aside/Aside';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Blogs = () => {
     const [data, setData] = useState([]);
     const [bookmark, setBookmark] = useState([]);
 
+    const notify = (msg) => toast(msg);
+
     const handleAddToBookmark = (blog) => {
         let newBlog = [];
         const exist = bookmark.find(blg => blg.id === blog.id);
 
-        if(!exist) {
+        if (!exist) {
             newBlog = [...bookmark, blog];
+            notify("Blogs add in bookmark");
         } else {
             const remaining = bookmark.filter(bg => bg.id !== blog.id);
             newBlog = [...remaining];
-            console.log("rmaining", remaining)
+            notify("Blogs remove from bookmark");
         }
         setBookmark(newBlog);
     }
@@ -35,11 +40,13 @@ const Blogs = () => {
                         key={blog.id}
                         handleAddToBookmark={handleAddToBookmark}
                         blog={blog}
-                        >
+                    >
                     </Blog>)
                 }
+                <ToastContainer />
+
             </div>
-                <Aside bookmark={bookmark}></Aside>
+            <Aside bookmark={bookmark}></Aside>
         </main>
     );
 };
